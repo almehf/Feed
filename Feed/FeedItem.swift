52057d448e363e973847917cb0e09c8524e92206
Fahad <almehawas@gmail.com>
2020-09-28 14:13:48 -0500

    Created Feed model

---
+++ Feed/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  Feed
+//
+//  Created by Fahad Almehawas  on 9/28/20.
+//  Copyright © 2020 Fahad Al. All rights reserved.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let uid: UUID
+    let description: String?
+    let location: String?
+    let ImageURL: String?
+}
