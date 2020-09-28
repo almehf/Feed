c9e1c0510d6f41329d4715e5911fb70183c0eee4
Fahad <almehawas@gmail.com>
2020-09-28 16:11:23 -0500

    Requested data from URL on 'Feed' & move test logic to a separate type 'HTTPClientSpy'

--- FeedTests/RemoteFeedFetcherTests.swift
+++ FeedTests/RemoteFeedFetcherTests.swift
@@ -10,21 +10,43 @@ import XCTest
 
 class RemoteFeedFetcher {
     func fetch() {
-        
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClient {
+    static var shared = HTTPClient()
+    func get(from url:URL?) {}
+    
+}
+
+//Swap the HTTPClient shared instance with spy subclass durning tests.
+
+class HTTPClientSpy: HTTPClient {
+    override func get(from url:URL?) {
+        requestedURL = url
+    }
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedFetcherTests: XCTestCase {
     
     func test_init_NotRequestedFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedFetcher()
-    
+        
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_fetch_reuqestedDataFromURL() {
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
+        let sut = RemoteFeedFetcher()
+        
+        sut.fetch()
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
