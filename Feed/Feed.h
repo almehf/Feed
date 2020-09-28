0433b171d3d6dbcb16e5145667b7661987c775a9
Fahad <almehawas@gmail.com>
2020-09-28 13:59:41 -0500

    Initial Commit

---
+++ Feed/Feed.h
@@ -0,0 +1,19 @@
+//
+//  Feed.h
+//  Feed
+//
+//  Created by Fahad Almehawas  on 9/28/20.
+//  Copyright © 2020 Fahad Al. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Feed.
+FOUNDATION_EXPORT double FeedVersionNumber;
+
+//! Project version string for Feed.
+FOUNDATION_EXPORT const unsigned char FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feed/PublicHeader.h>
+
+
